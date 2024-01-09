e5f5ff10ef170680fec5d83d8a649c8d7a8f0528
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-01-10 10:36:50 +1100

    add banner

---
+++ layouts/components/banner_559.svelte
@@ -0,0 +1,23 @@
+<!-- ============================================ -->
+<!--                   Banner                     -->
+<!-- ============================================ -->
+
+<div id="banner-559">
+    <div class="cs-container">
+        <span class="cs-int-topper">Building Communities</span>
+        <span class="cs-int-title">About</span>
+        <div class="cs-breadcrumbs">
+            <a href="." class="cs-link">Home</a>
+            <a href="." class="cs-link cs-active">About</a>
+        </div>
+    </div>
+    <!--Background Image-->
+    <picture class="cs-background">
+        <!--Mobile Image-->
+        <source media="(max-width: 600px)" srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Hero/dna-m.jpg">
+        <!--Tablet and above Image-->
+        <source media="(min-width: 601px)" srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Hero/dna.jpg">
+        <img loading="lazy" decoding="async" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Hero/dna.jpg" alt="dna" width="1280" height="568" aria-hidden="true">
+    </picture>
+</div>
+                                
