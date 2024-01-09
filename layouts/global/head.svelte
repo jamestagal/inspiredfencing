4bda469e9e846c475f9884fc6d1cef0eb0293fbb
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-01-10 10:38:02 +1100

    remove leading forward slash from env entrypoints

--- layouts/global/head.svelte
+++ layouts/global/head.svelte
@@ -1,22 +1,27 @@
 <script>
-  export let title, env;
+  export let env;
 </script>
 
 <head>
-  <meta charset='utf-8'>
-  <meta name='viewport' content='width=device-width,initial-scale=1'>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width,initial-scale=1" />
 
-  <title>{ title } | Inspired Fencing</title>
-  <base href="{ env.baseurl }">
-  <script type="module" src={`/${env.entrypointJS}/core/main.js`}></script>
+  <title>Home | Inspired Fencing</title>
+  <base href={env.baseurl} />
+  <script type="module" src={`${env.entrypointJS}/core/main.js`}></script>
   <script type="module" src="js/nav.js"></script>
-    <!-- Custom styles -->
+  <!-- Custom styles -->
+  <link rel="stylesheet" href="critical.css" />
+  <link rel="stylesheet" href="global.css" />
+  <noscript>
     <link rel="stylesheet" href="global.css" />
-  <link rel="icon" type="image/svg+xml" href="/media/logo.svg">
-  <link rel='stylesheet' href={`/${env.entrypointJS}/bundle.css`}>
+  </noscript>
+
+  <link rel="icon" type="image/svg+xml" href="media/logo.svg" />
+  <link rel="stylesheet" href={`${env.entrypointJS}/bundle.css`} />
   <link
-    rel="preload"
-    href="fonts/roboto-v29-latin-regular.woff2 "
+    rel="stylesheet preload"
+    href="fonts/roboto-v29-latin-regular.woff2"
     as="font"
     type="font/woff2"
     crossorigin
