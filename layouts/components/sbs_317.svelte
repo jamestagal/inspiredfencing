deb0f871963f3769fb24da21c7c3bf6558ba8ba5
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-01-10 09:57:54 +1100

    add side by side

---
+++ layouts/components/sbs_317.svelte
@@ -0,0 +1,62 @@
+<!-- ============================================ -->
+<!--                 Side By Side                 -->
+<!-- ============================================ -->
+
+<section id="sbs-317">
+    <div class="cs-container">
+        <!-- Left Image Section -->
+        <picture class="cs-picture">
+            <source media="(max-width: 600px)" srcset="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/consult-m.jpg">
+            <source media="(min-width: 601px)" srcset="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/consult.jpg">
+            <img loading="lazy" decoding="async" src="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/consult.jpg" alt="consult" width="520" height="680" aria-hidden="true">
+        </picture>
+        <!-- Right Content Section-->
+        <div class="cs-content">
+            <span class="cs-topper">Best Experience</span>
+            <h2 class="cs-title">Find Us What We Actually Do For Giving Complete Solutions.</h2>
+            <p class="cs-text">
+                In consequat tincidunt turpis sit amet imperdiet. Praesent Class officelan nonatoureanor mauris laoreet, iaculis libero quis. Curabitur et tempus eri consequat tincidunt turpis sit amet imperdiet. Praesent nonatourean olei aptent taciti sociosqu ad litora torquent per.
+            </p>
+            <a class="cs-button-solid" aria-label="learn more about our programs" href="">Learn More</a>
+
+            <!--SVG Circles-->
+            <div class="cs-circles">
+                <div class="cs-circle">
+                    <!--Wrapped in a div so mobile positioning is easier-->
+                    <div class="cs-ring-wrapper">
+                        <span class="cs-number">90%</span>
+                        <!--Change the --ratio to the decimal percentage of this circle-->
+                        <svg class="cs-ring" style="--ratio: 0.90">
+                            <circle cx="50%" cy="50%" r="50%"/>
+                            <circle cx="50%" cy="50%" r="50%"/>
+                        </svg>
+                    </div>
+                    <span class="cs-desc">Case Win</span>
+                </div>
+                <div class="cs-circle">
+                    <div class="cs-ring-wrapper">
+                        <span class="cs-number">75%</span>
+                        <!--Change the --ratio to the decimal percentage of this circle-->
+                        <svg class="cs-ring" style="--ratio: 0.75">
+                            <circle cx="50%" cy="50%" r="50%"/>
+                            <circle cx="50%" cy="50%" r="50%"/>
+                        </svg>
+                    </div>
+                    <span class="cs-desc">Settle</span>
+                </div>
+                <div class="cs-circle">
+                    <div class="cs-ring-wrapper">
+                        <span class="cs-number">30%</span>
+                        <!--Change the --ratio to the decimal percentage of this circle-->
+                        <svg class="cs-ring" style="--ratio: 0.30">
+                            <circle cx="50%" cy="50%" r="50%"/>
+                            <circle cx="50%" cy="50%" r="50%"/>
+                        </svg>
+                    </div>
+                    <span class="cs-desc">Case Pending</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+                                
