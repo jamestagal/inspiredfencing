2a9557c7c9cc93435dd1553352bc99151d59a5f9
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-01-10 09:58:15 +1100

    add reverse side by side

---
+++ layouts/components/sbsr_298.svelte
@@ -0,0 +1,39 @@
+<!-- ============================================ -->
+<!--            Side By Side Reverse              -->
+<!-- ============================================ -->
+
+<section id="sbsr-298">
+    <div class="cs-container">
+        <!-- Left Image Section -->
+        <div class="cs-image-group">
+            <!--Floating Box-->
+            <div class="cs-box">
+                <span class="cs-number">14</span>
+                <span class="cs-desc">years of practicing</span>
+                <!--Background image for cs-box-->
+                <picture class="cs-box-picture">
+                    <source media="(max-width: 600px)" srcset="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/justice-m.jpg">
+                    <source media="(min-width: 601px)" srcset="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/justice.jpg">
+                    <img loading="lazy" decoding="async" src="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/justice.jpg" alt="greek pillars" width="521" height="672" aria-hidden="true">
+                </picture>
+            </div>
+            <!--Big Background Image-->
+            <picture class="cs-picture">
+                <source media="(max-width: 600px)" srcset="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/pillars-m.jpg">
+                <source media="(min-width: 601px)" srcset="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/pillars.jpg">
+                <img loading="lazy" decoding="async" src="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/pillars.jpg" alt="greek pillars" width="521" height="672" aria-hidden="true">
+            </picture>
+        </div>
+        <!-- Right Content Section-->
+        <div class="cs-content">
+            <span class="cs-topper">Who We Are</span>
+            <h2 class="cs-title">Good People, Great Lawyers</h2>
+            <p class="cs-text">
+                If you are facing criminal charges, it is crucial to have effective representation immediately. Not only during trial, but also during the pretrial phase as well. At Bernstein Attorneys, our London criminal defense lawyer give your case our full time and attention, to ensure you are treated fairly under the justice system.
+            </p>
+            <p class="cs-text">Call to ask any question: <a class="cs-link" href="tel:">(555) 455-4555</a></p>
+            <a class="cs-button-solid" aria-label="learn more about our programs" href="">Learn More</a>
+        </div>
+    </div>
+</section>
+                                
