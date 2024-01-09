3ed04150071f68f4badec49f33b760d5b3b7cdad
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-01-10 09:55:53 +1100

    add cta

---
+++ layouts/components/cta_697.svelte
@@ -0,0 +1,15 @@
+<!-- ============================================ -->
+<!--                     CTA                      -->
+<!-- ============================================ -->
+
+<section id="cta-697">
+    <div class="cs-container">
+        <div class="cs-content">
+            <h2 class="cs-title">Have Doubts? Get Your Queries Solved by Our Experts</h2>
+            <a href="." class="cs-button-solid">Get In Touch</a>
+        </div>
+    </div>
+    <!--SVG Waves Background-->
+    <img class="cs-background" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images%2FGraphics%2Fwaves-3.svg" alt="wave graphic" width="1920" height="318" loading="lazy" decoding="async" aria-hidden="true">
+</section>
+                                
