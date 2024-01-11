9f8395f8af6265e991a078d960782094d17ca055
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-01-11 11:59:56 +1100

    add contact page and comp

---
+++ layouts/components/contact_486.svelte
@@ -0,0 +1,49 @@
+<!-- ============================================ -->
+<!--                    Form                      -->
+<!-- ============================================ -->
+
+<section id="cs-contact-486">
+    <div class="cs-container">
+        <div class="cs-content">
+            <h2 class="cs-title">Talk to us about your <span class="cs-color">fencing plan!</span></h2>
+            <p class="cs-text">
+                We would love to hear about your project and our team is ready to discuss your needs, provide a free consultation, and offer expert guidance to bring your vision to life. Contact us today and let us transform your home with our professional service. Please fill the form on the right side to schedule a call or discussion and meeting on the your convenient time.
+            </p>
+            <span class="cs-heading">Any problem about your booking?</span>
+            <span class="cs-heading">You can contact us on :</span>
+            <a href="tel:219-555-4545" class="cs-link">0416 186 911</a>
+            <a href="mailto:inspiredfencing@outlook.com" class="cs-link">inspiredfencing@outlook.com</a>
+        </div>
+        <!--Form-->
+        <form id="cs-form-486" name="Contact Form" method="post">
+            <h3 class="cs-h3">Get In Touch</h3>
+            <label class="cs-label">
+                Name
+                <input class="cs-input" required type="text" id="name-486" name="name" placeholder="Name">
+            </label>
+            <label class="cs-label">
+                Phone
+                <input class="cs-input" required type="text" id="phone-486" name="phone" placeholder="Phone">
+            </label>
+            <label class="cs-label">
+                Email
+                <input class="cs-input" required type="text" id="email-486" name="email" placeholder="Email">
+            </label>
+            <label class="cs-label">
+                How Did You Find Us
+                <input class="cs-input" type="text" id="find-486" name="find-us" placeholder="How did you find us?">
+            </label>
+            <label class="cs-label cs-label-message">
+                What do you need help with?
+                <select name="cars" id="cs-select-486">
+                    <option value="full renovation">New fence / gate</option>
+                    <option value="sod install">Fence / gate repair or replacement</option>
+                    <option value="yard cleanup">Fence and Waste Removal</option>
+                    <option value="landscape design">Handyman and Garden Maintenance</option>
+                </select>
+            </label>
+            <button class="cs-button-solid cs-submit" type="submit">Book a Consult</button>
+        </form>
+    </div>
+</section>
+                                
