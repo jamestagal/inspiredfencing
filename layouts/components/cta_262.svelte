95b9328a35944bf2a39d3217f559647d81196356
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-01-11 12:01:26 +1100

    add new cta comp

---
+++ layouts/components/cta_262.svelte
@@ -0,0 +1,27 @@
+<!-- ============================================ -->
+<!--                    CTA                       -->
+<!-- ============================================ -->
+
+<section id="cta-262">
+    <div class="cs-container">
+        <div class="cs-left-section">
+            <h2 class="cs-title">Get in touch for a quote</h2>
+            <a href="contact" class="cs-button-solid">Request a quote</a>
+        </div>
+        <div class="cs-content">
+            <span class="cs-header">Available Hours</span>
+            <p class="cs-p"> 
+                Monday - Friday: 8am-6pm
+            </p>
+            <p class="cs-p">
+                Appointments Preferred.
+            </p>
+        </div>
+        <picture class="cs-background">
+            <source media="(max-width: 600px)" srcset="media/images/Feature_Colourbond.jpeg">
+            <source media="(min-width: 601px)" srcset="media/images/Feature_Colourbond.jpeg">
+            <img aria-hidden="true" loading="lazy" decoding="async" src="media/images/Feature_Colourbond.jpeg" alt="appliance" width="275" height="132">
+        </picture>
+    </div>
+</section>
+                                
