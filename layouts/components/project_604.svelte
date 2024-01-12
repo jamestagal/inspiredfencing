d59ed0fbe2a2ec2202bb18a84b4dd56bb2d13f04
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-01-12 19:50:20 +1100

    create new project's page

---
+++ layouts/components/project_604.svelte
@@ -0,0 +1,77 @@
+<!-- ============================================ -->
+<!--                  Projects                    -->
+<!-- ============================================ -->
+
+<section id="projects-604">
+    <div class="cs-container">
+        <div class="cs-content">
+            <div class="cs-flex-group">
+                <span class="cs-topper">Our Services</span>
+                <h2 class="cs-title">Some of our residential fencing projects</h2>
+            </div>
+            <p class="cs-text">
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.
+            </p>
+        </div>
+        <ul class="cs-card-group">
+            <li class="cs-item">
+                <a class="cs-link" href=".">
+                    <div class="cs-info">
+                        <picture class="cs-background">
+                            <!--Mobile Image-->
+                            <source media="(max-width: 600px)" srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Services/bathroom1.jpg">
+                            <!--Tablet and above Image-->
+                            <source media="(min-width: 601px)" srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Services/bathroom1.jpg">
+                            <img loading="lazy" decoding="async" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Services/bathroom1.jpg" alt="bathroom" width="413" height="450">
+                        </picture>
+                        <span class="cs-header">Project 1</span>
+                        <p class="cs-item-text">
+                            We received a call from an existing client in November 2018. 
+                        </p>
+                        <span class="cs-number">01</span>
+                    </div>
+                    <h3 class="cs-h3">Colorbond Project</h3>
+                </a>
+            </li>
+            <li class="cs-item">
+                <a class="cs-link" href=".">
+                    <div class="cs-info">
+                        <picture class="cs-background">
+                            <!--Mobile Image-->
+                            <source media="(max-width: 600px)" srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Services/bathroom1.jpg">
+                            <!--Tablet and above Image-->
+                            <source media="(min-width: 601px)" srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Services/bathroom1.jpg">
+                            <img loading="lazy" decoding="async" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Services/bathroom1.jpg" alt="bathroom" width="413" height="450">
+                        </picture>
+                        <span class="cs-header">Project 2</span>
+                        <p class="cs-item-text">
+                            Back in October 2018 we tackled a unique job in Rye.
+                        </p>
+                        <span class="cs-number">02</span>
+                    </div>
+                    <h3 class="cs-h3">Driveway double gates Project</h3>
+                </a>
+            </li>
+            <li class="cs-item">
+                <a class="cs-link" href=".">
+                    <div class="cs-info">
+                        <picture class="cs-background">
+                            <!--Mobile Image-->
+                            <source media="(max-width: 600px)" srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Services/bathroom1.jpg">
+                            <!--Tablet and above Image-->
+                            <source media="(min-width: 601px)" srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Services/bathroom1.jpg">
+                            <img loading="lazy" decoding="async" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Services/bathroom1.jpg" alt="bathroom" width="413" height="450">
+                        </picture>
+                        <span class="cs-header">Project 3</span>
+                        <p class="cs-item-text">
+                            In December 2018 we took on the challenge of a 40-metre brush fence at a holiday home in Flinders.
+                        </p>
+                        <span class="cs-number">03</span>
+                    </div>
+                    <h3 class="cs-h3">Brush Fence Project</h3>
+                </a>
+            </li>
+        </ul>
+    </div>
+</section>
+                                
