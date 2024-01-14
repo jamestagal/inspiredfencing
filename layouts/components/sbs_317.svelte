50fed97b8aa0de853d2c4fbdc55696bf9a276dce
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-01-14 20:01:19 +1100

    move content to data source

--- layouts/components/sbs_317.svelte
+++ layouts/components/sbs_317.svelte
@@ -1,3 +1,6 @@
+<script>
+    export let subtitle, title, description, image, image_m, image_t, button;
+</script>
 <!-- ============================================ -->
 <!--                 Side By Side                 -->
 <!-- ============================================ -->
@@ -6,26 +9,29 @@
     <div class="cs-container">
         <!-- Left Image Section -->
         <picture class="cs-picture">
-            <source media="(max-width: 600px)" srcset="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/consult-m.jpg">
-            <source media="(min-width: 601px)" srcset="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/consult.jpg">
-            <img loading="lazy" decoding="async" src="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/consult.jpg" alt="consult" width="520" height="680" aria-hidden="true">
+            <source media="(max-width: 600px)" srcset="{image_m.url}">
+            <source media="(min-width: 601px)" srcset="{image_t.url}">
+            <img loading="lazy" decoding="async" src="{image.url}" alt="{image.alt}" width="520" height="680" aria-hidden="true">
         </picture>
         <!-- Right Content Section-->
         <div class="cs-content">
-            <span class="cs-topper">Best Experience</span>
-            <h2 class="cs-title">Find Us What We Actually Do For Giving Complete Solutions.</h2>
+            <span class="cs-topper">{subtitle}</span>
+            <h2 class="cs-title">{title}</h2>
+            {#each description as d}
             <p class="cs-text">
-                In consequat tincidunt turpis sit amet imperdiet. Praesent Class officelan nonatoureanor mauris laoreet, iaculis libero quis. Curabitur et tempus eri consequat tincidunt turpis sit amet imperdiet. Praesent nonatourean olei aptent taciti sociosqu ad litora torquent per.
+                {@html d.paragraph1}
+                <br><br>
+                {@html d.paragraph2}
             </p>
-            <a class="cs-button-solid" aria-label="learn more about our programs" href="">Learn More</a>
-
-            <!--SVG Circles-->
+            {/each}
+            <a class="cs-button-solid" aria-label="learn more about our programs" href="{button.url}">{button.title}</a>
+           <!--  SVG Circles--
             <div class="cs-circles">
                 <div class="cs-circle">
-                    <!--Wrapped in a div so mobile positioning is easier-->
+                    <!--Wrapped in a div so mobile positioning is easier--
                     <div class="cs-ring-wrapper">
                         <span class="cs-number">90%</span>
-                        <!--Change the --ratio to the decimal percentage of this circle-->
+                        <!--Change the --ratio to the decimal percentage of this circle--
                         <svg class="cs-ring" style="--ratio: 0.90">
                             <circle cx="50%" cy="50%" r="50%"/>
                             <circle cx="50%" cy="50%" r="50%"/>
@@ -36,7 +42,7 @@
                 <div class="cs-circle">
                     <div class="cs-ring-wrapper">
                         <span class="cs-number">75%</span>
-                        <!--Change the --ratio to the decimal percentage of this circle-->
+                        <!--Change the --ratio to the decimal percentage of this circle-
                         <svg class="cs-ring" style="--ratio: 0.75">
                             <circle cx="50%" cy="50%" r="50%"/>
                             <circle cx="50%" cy="50%" r="50%"/>
@@ -47,7 +53,7 @@
                 <div class="cs-circle">
                     <div class="cs-ring-wrapper">
                         <span class="cs-number">30%</span>
-                        <!--Change the --ratio to the decimal percentage of this circle-->
+                        <!--Change the --ratio to the decimal percentage of this circle--
                         <svg class="cs-ring" style="--ratio: 0.30">
                             <circle cx="50%" cy="50%" r="50%"/>
                             <circle cx="50%" cy="50%" r="50%"/>
@@ -55,7 +61,7 @@
                     </div>
                     <span class="cs-desc">Case Pending</span>
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
 </section>
