f85319996de331c34e8b8a5d98ee645d016c8510
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-01-14 22:51:00 +1100

    add new projects section and reference images

---
+++ layouts/components/services_1169.svelte
@@ -0,0 +1,132 @@
+<!-- ============================================ -->
+<!--                  Services                    -->
+<!-- ============================================ -->
+
+<section id="services-1169">
+    <div class="cs-container">
+      <div class="cs-content">
+        <span class="cs-topper">Our Projects</span>
+        <h2 class="cs-title">Featured Projects</h2>
+        <p class="cs-text">
+          Inspired Fencing offers a wide variety of fencing products to suit your style and environment. Read about some of our feeatured fence and gate jobs to see the quality products we use and our workmanship.
+        </p>
+      </div>
+      <ul class="cs-card-group">
+        <li class="cs-item">
+          <picture class="cs-picture">
+            <!--Mobile Image-->
+            <source
+                  media="(max-width: 600px)"
+                  srcset="media/images/Project_Colourbond_01.jpeg">
+            <!--Tablet and above Image-->
+            <source
+                  media="(min-width: 601px)"
+                  srcset="media/images/Project_Colourbond_01.jpeg">
+            <img
+                  loading="lazy"
+                  decoding="async"
+                  src="media/images/Project_Colourbond_01.jpeg"
+                  alt="mechanic"
+                  width="413"
+                  height="240"
+                  aria-hidden="true">
+          </picture>
+          <div class="cs-info">
+            <h3 class="cs-h3">Colorbond</h3>
+            <p class="cs-item-text">
+              What is Check Engine Light? There are many variations of passages of Lorem Ipsum available, but the majority have...
+            </p>
+            <a href="." class="cs-link">
+              Read More
+              <img
+                    class="cs-arrow"
+                    loading="lazy"
+                    decoding="async"
+                    src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/red-arrow-right.svg"
+                    alt="icon"
+                    width="20"
+                    height="20"
+                    aria-hidden="true">
+            </a>
+          </div>
+        </li>
+        <li class="cs-item">
+          <picture class="cs-picture">
+            <!--Mobile Image-->
+            <source
+                  media="(max-width: 600px)"
+                  srcset="media/images/Project_Driveway_01.jpeg">
+            <!--Tablet and above Image-->
+            <source
+                  media="(min-width: 601px)"
+                  srcset="media/images/Project_Driveway_01.jpeg">
+            <img
+                  loading="lazy"
+                  decoding="async"
+                  src="media/images/Project_Driveway_01.jpeg"
+                  alt="mechanic"
+                  width="413"
+                  height="240"
+                  aria-hidden="true">
+          </picture>
+          <div class="cs-info">
+            <h3 class="cs-h3">Driveway double gates</h3>
+            <p class="cs-item-text">
+              Our featured services Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur...
+            </p>
+            <a href="." class="cs-link">
+              Read More
+              <img
+                    class="cs-arrow"
+                    loading="lazy"
+                    decoding="async"
+                    src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/red-arrow-right.svg"
+                    alt="icon"
+                    width="20"
+                    height="20"
+                    aria-hidden="true">
+            </a>
+          </div>
+        </li>
+        <li class="cs-item">
+          <picture class="cs-picture">
+            <!--Mobile Image-->
+            <source
+                  media="(max-width: 600px)"
+                  srcset="media/images/Project_Brush_01.jpeg">
+            <!--Tablet and above Image-->
+            <source
+                  media="(min-width: 601px)"
+                  srcset="media/images/Project_Brush_01.jpeg">
+            <img
+                  loading="lazy"
+                  decoding="async"
+                  src="media/images/Project_Brush_01.jpeg"
+                  alt="mechanic"
+                  width="413"
+                  height="240"
+                  aria-hidden="true">
+          </picture>
+          <div class="cs-info">
+            <h3 class="cs-h3">Bruch fence</h3>
+            <p class="cs-item-text">
+              Our services Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni...
+            </p>
+            <a href="." class="cs-link">
+              Read More
+              <img
+                    class="cs-arrow"
+                    loading="lazy"
+                    decoding="async"
+                    src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/red-arrow-right.svg"
+                    alt="icon"
+                    width="20"
+                    height="20"
+                    aria-hidden="true">
+            </a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
+                                
