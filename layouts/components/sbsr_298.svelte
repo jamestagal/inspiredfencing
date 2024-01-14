15f719c771dec378eab4c0266cec3d4059320b24
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-01-14 20:01:32 +1100

    move content to data source

--- layouts/components/sbsr_298.svelte
+++ layouts/components/sbsr_298.svelte
@@ -1,3 +1,6 @@
+<script>
+    export let subtitle, title, description, box, image, image_m, image_t, button, contact;
+</script>
 <!-- ============================================ -->
 <!--            Side By Side Reverse              -->
 <!-- ============================================ -->
@@ -8,31 +11,35 @@
         <div class="cs-image-group">
             <!--Floating Box-->
             <div class="cs-box">
-                <span class="cs-number">14</span>
-                <span class="cs-desc">years of practicing</span>
+                <span class="cs-number">{box.number}</span>
+                <span class="cs-desc">{box.description}</span>
                 <!--Background image for cs-box-->
                 <picture class="cs-box-picture">
-                    <source media="(max-width: 600px)" srcset="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/justice-m.jpg">
-                    <source media="(min-width: 601px)" srcset="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/justice.jpg">
-                    <img loading="lazy" decoding="async" src="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/justice.jpg" alt="greek pillars" width="521" height="672" aria-hidden="true">
+                    <source media="(max-width: 600px)" srcset="{image_m.url}">
+                    <source media="(min-width: 601px)" srcset="{image_t.url}">
+                    <img loading="lazy" decoding="async" src="{image.url}" alt="{image.alt}" width="521" height="672" aria-hidden="true">
                 </picture>
             </div>
             <!--Big Background Image-->
             <picture class="cs-picture">
-                <source media="(max-width: 600px)" srcset="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/pillars-m.jpg">
-                <source media="(min-width: 601px)" srcset="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/pillars.jpg">
-                <img loading="lazy" decoding="async" src="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/pillars.jpg" alt="greek pillars" width="521" height="672" aria-hidden="true">
+                <source media="(max-width: 600px)" srcset="{image_m.url}">
+                <source media="(min-width: 601px)" srcset="{image_t.url}">
+                <img loading="lazy" decoding="async" src="{image.url}" alt="{image.alt}" width="521" height="672" aria-hidden="true">
             </picture>
         </div>
         <!-- Right Content Section-->
         <div class="cs-content">
-            <span class="cs-topper">Who We Are</span>
-            <h2 class="cs-title">Good People, Great Lawyers</h2>
+            <span class="cs-topper">{subtitle}</span>
+            <h2 class="cs-title">{title}</h2>
+            {#each description as d}
             <p class="cs-text">
-                If you are facing criminal charges, it is crucial to have effective representation immediately. Not only during trial, but also during the pretrial phase as well. At Bernstein Attorneys, our London criminal defense lawyer give your case our full time and attention, to ensure you are treated fairly under the justice system.
+                {@html d.paragraph1}
+                <br><br>
+                {@html d.paragraph2}
             </p>
-            <p class="cs-text">Call to ask any question: <a class="cs-link" href="tel:">(555) 455-4555</a></p>
-            <a class="cs-button-solid" aria-label="learn more about our programs" href="">Learn More</a>
+            {/each}
+            <p class="cs-text">{contact.text} <a class="cs-link" href="tel:">{contact.number}</a></p>
+            <a class="cs-button-solid" aria-label="learn more about our programs" href="{button.url}">{button.title}</a>
         </div>
     </div>
 </section>
