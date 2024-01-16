f82c3fc7cf72911223fe46aa7b7818e2d9427447
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-01-17 10:34:53 +1100

    add new comp sbs 1357

---
+++ layouts/components/sbs_1357.svelte
@@ -0,0 +1,34 @@
+<!-- ============================================ -->
+<!--                 Side By Side                 -->
+<!-- ============================================ -->
+
+<section id="sbs-1357">
+    <div class="cs-container">
+        <picture class="cs-picture cs-picture1">
+            <!--Mobile Image-->
+            <source media="(max-width: 600px)" srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/computer1.jpg">
+            <!--Tablet and above Image-->
+            <source media="(min-width: 601px)" srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/computer1.jpg">
+            <img loading="lazy" decoding="async" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/computer1.jpg" alt="meeting" width="305" height="339">
+        </picture>
+        <div class="cs-content">
+            <span class="cs-topper">Brush Fence</span>
+            <h2 class="cs-title">A Deam fence came true</h2>
+            <h3 class="cs-h3">
+                We’re committed to delivering the fence you love
+            </h3>
+            <p class="cs-text">
+                In December 2018, we took on the challenge of a 40-metre brush fence at a holiday home in Flinders. This property was undergoing major renovations at the time and the owner has always wanted a brush fence on this property to compliment the coastal beach location.
+            </p>
+            <a href="contact" class="cs-button-solid">Get a quote</a>
+        </div>
+        <picture class="cs-picture cs-picture2">
+            <!--Mobile Image-->
+            <source media="(max-width: 600px)" srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/computer2.jpg">
+            <!--Tablet and above Image-->
+            <source media="(min-width: 601px)" srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/computer2.jpg">
+            <img loading="lazy" decoding="async" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/computer2.jpg" alt="meeting" width="305" height="339">
+        </picture>
+    </div>
+    <div class="cs-bubbles" aria-hidden="true"></div>
+</section>
