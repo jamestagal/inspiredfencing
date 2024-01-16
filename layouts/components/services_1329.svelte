6bb1a7ee779e1b27053f3683ac18817e10a2264d
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-01-17 10:36:54 +1100

    add new comp services 1329

---
+++ layouts/components/services_1329.svelte
@@ -0,0 +1,127 @@
+<!-- ============================================ -->
+<!--                    Services 1329            -->
+<!-- ============================================ -->
+
+<section id="services-1329">
+  <div class="cs-container">
+    <div class="cs-content">
+      <div class="cs-flex-group">
+        <span class="cs-topper">Our Services</span>
+        <h2 class="cs-title">Additional Services</h2>
+      </div>
+      <p class="cs-text">
+        Inspire Fencing offer 3 additional services. <br><strong>1. Fence Repair & Replacement </strong><br> <strong>2. Fence and Waste Removal</strong><br> <strong>3. Handyman and Garden Maintenance </strong> </p>
+    </div>
+    <ul class="cs-card-group">
+      <li class="cs-item">
+        <a href="." class="cs-link">
+          <span class="cs-number">01</span>
+          <h3 class="cs-h3">Fence Repair & Replacement</h3>
+          <p class="cs-item-text">We consider all the drivers</p>
+        </a>
+        <picture class="cs-picture">
+          <source
+            media="(max-width: 600px)"
+            srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/MISC/finance-screen.png"
+          />
+          <source
+            media="(min-width: 601px)"
+            srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/MISC/finance-screen.png"
+          />
+          <img
+            decoding="async"
+            src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/MISC/finance-screen.png"
+            alt="finance"
+            width="413"
+            height="374"
+            aria-hidden="true"
+          />
+        </picture>
+        <!--On Hover Image-->
+        <img
+          class="cs-arrow"
+          loading="lazy"
+          decoding="async"
+          src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/Graphics/Icon.svg"
+          alt="arrow"
+          width="412"
+          height="249"
+          aria-hidden="true"
+        />
+      </li>
+      <li class="cs-item">
+        <a href="." class="cs-link">
+          <span class="cs-number">02</span>
+          <h3 class="cs-h3">Fence and Waste Removal</h3>
+          <p class="cs-item-text">We consider all the drivers</p>
+        </a>
+        <picture class="cs-picture">
+          <source
+            media="(max-width: 600px)"
+            srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/MISC/finance-reading.png"
+          />
+          <source
+            media="(min-width: 601px)"
+            srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/MISC/finance-reading.png"
+          />
+          <img
+            decoding="async"
+            src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/MISC/finance-reading.png"
+            alt="finance"
+            width="413"
+            height="374"
+            aria-hidden="true"
+          />
+        </picture>
+        <!--On Hover Image-->
+        <img
+          class="cs-arrow"
+          loading="lazy"
+          decoding="async"
+          src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/Graphics/Icon.svg"
+          alt="arrow"
+          width="412"
+          height="249"
+          aria-hidden="true"
+        />
+      </li>
+      <li class="cs-item">
+        <a href="." class="cs-link">
+          <span class="cs-number">03</span>
+          <h3 class="cs-h3">Handyman and Garden Maintenance</h3>
+          <p class="cs-item-text">We consider all the drivers</p>
+        </a>
+        <picture class="cs-picture">
+          <source
+            media="(max-width: 600px)"
+            srcset="https://csimg.nyc3.cdn.digita loceanspaces.com/Images/MISC/finance-laptop.png"
+          />
+          <source
+            media="(min-width: 601px)"
+            srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/MISC/finance-laptop.png"
+          />
+          <img
+            decoding="async"
+            src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/MISC/finance-laptop.png"
+            alt="finance"
+            width="413"
+            height="374"
+            aria-hidden="true"
+          />
+        </picture>
+        <!--On Hover Image-->
+        <img
+          class="cs-arrow"
+          loading="lazy"
+          decoding="async"
+          src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/Graphics/Icon.svg"
+          alt="arrow"
+          width="412"
+          height="249"
+          aria-hidden="true"
+        />
+      </li>
+    </ul>
+    <a href="contact" class="cs-button-solid">Get in Touch</a>
+  </div>
+</section>
