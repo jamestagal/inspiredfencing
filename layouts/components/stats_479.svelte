f5d7a92fe7e08a5a4a87fa59bee01daba7792838
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-01-17 10:37:16 +1100

    add replacement pattern

--- layouts/components/stats_479.svelte
+++ layouts/components/stats_479.svelte
@@ -6,19 +6,19 @@
     <div class="cs-container">
         <ul class="cs-card-group">
             <li class="cs-item">
-                <img class="cs-icon" aria-hidden="true" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Stats/codestitch.svg" loading="lazy" decoding="async" alt="icon" width="76" height="66">
-                <span class="cs-number">100+</span>
-                <span class="cs-desc">Landscape Renovations Completed</span>
-            </li>
-            <li class="cs-item">
-                <img class="cs-icon" aria-hidden="true" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Stats/codestitch.svg" loading="lazy" decoding="async" alt="icon" width="76" height="66">
-                <span class="cs-number">100%</span>
-                <span class="cs-desc">Guaranteed Customer Satisfaction</span>
-            </li>
-            <li class="cs-item">
-                <img class="cs-icon" aria-hidden="true" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Stats/codestitch.svg" loading="lazy" decoding="async" alt="icon" width="76" height="66">
+                <img class="cs-icon" aria-hidden="true" src="media/svg/stat1.svg" loading="lazy" decoding="async" alt="icon" width="76" height="66">
                 <span class="cs-number">5/5</span>
-                <span class="cs-desc">From 34 Google Ratings</span>
+                <span class="cs-desc">Rating</span>
+            </li>
+            <li class="cs-item">
+                <img class="cs-icon" aria-hidden="true" src="media/svg/stat2.svg" loading="lazy" decoding="async" alt="icon" width="76" height="66">
+                <span class="cs-number">100%</span>
+                <span class="cs-desc">Satisfaction</span>
+            </li>
+            <li class="cs-item">
+                <img class="cs-icon" aria-hidden="true" src="media/svg/stat3.svg" loading="lazy" decoding="async" alt="icon" width="76" height="66">
+                <span class="cs-number">10+</span>
+                <span class="cs-desc">Years Experience</span>
             </li>
         </ul>
     </div>
