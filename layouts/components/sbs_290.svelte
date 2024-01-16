1d3e30738b6fdaed06246d11900349250d6674eb
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-01-17 10:34:00 +1100

    add new comp sbs 290

---
+++ layouts/components/sbs_290.svelte
@@ -0,0 +1,63 @@
+<!-- ============================================ -->
+<!--                 Side By Side                 -->
+<!-- ============================================ -->
+
+<section id="sbs-290">
+    <div class="cs-container">
+        <!-- Left Image Section -->
+        <div class="cs-image-group">
+            <picture class="cs-picture cs-picture1">
+                <source media="(max-width: 600px)" srcset="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/people1.jpg">
+                <source media="(min-width: 601px)" srcset="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/people1.jpg">
+                <img loading="lazy" decoding="async" src="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/people1.jpg" alt="people in a meeting" width="398" height="393" aria-hidden="true">
+            </picture>
+            <picture class="cs-picture cs-picture2">
+                <source media="(max-width: 600px)" srcset="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/people2.jpg">
+                <source media="(min-width: 601px)" srcset="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/people2.jpg">
+                <img loading="lazy" decoding="async" src="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/people2.jpg" alt="people in a meeting" width="320" height="318" aria-hidden="true">
+            </picture>
+            <picture class="cs-picture cs-picture3">
+                <source media="(max-width: 600px)" srcset="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/people3.jpg">
+                <source media="(min-width: 601px)" srcset="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/people3.jpg">
+                <img loading="lazy" decoding="async" src="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/people3.jpg" alt="people in a meeting" width="325" height="322" aria-hidden="true">
+            </picture>
+            <picture class="cs-picture cs-picture4">
+                <source media="(max-width: 600px)" srcset="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/people4.jpg">
+                <source media="(min-width: 601px)" srcset="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/people4.jpg">
+                <img loading="lazy" decoding="async" src="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/people4.jpg" alt="people in a meeting" width="264" height="262" aria-hidden="true">
+            </picture>
+<!--             <a href="" class="cs-play-link" aria-label="play link">
+                <img loading="lazy" decoding="async" src="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/play.svg" alt="play icon" width="32" height="32" aria-hidden="true">
+            </a> -->
+        </div>
+        <!-- Right Content Section-->
+        <div class="cs-content">
+            <span class="cs-topper">Driveway double gates</span>
+            <h2 class="cs-title">Transforming a Holiday Home in Rye</h2>
+            <p class="cs-text">
+                Back in October 2018 we tackled a unique job in Rye. A Holiday home on a corner block was in desperate need of a new side fence and driveway gates for yard access, as well as a balustrade around an existing retaining wall in the yard. The completed fence was 12 metres long using standard palings at 1.65 M height with double plinth to compensate for the slope in the land. The double driveway gates had a steel frame and were finished in Colorbond Woodland Grey with a letterbox insert to match. For the balustrade we used treated pine plinth and posts for the most cost effective outcome for our client.
+            </p>
+            <p class="cs-text"><i>Features of double gates: </i></p>
+            <ul class="cs-ul">
+                <li class="cs-li">
+                    <img class="cs-li-img" loading="lazy" decoding="async" src="media/svg/checkmark-green.svg" alt="icon" width="26" height="28" aria-hidden="true">
+                    Easy access your private space
+                </li>
+                <li class="cs-li">
+                    <img class="cs-li-img" loading="lazy" decoding="async" src="media/svg/checkmark-green.svg" alt="icon" width="26" height="28" aria-hidden="true">
+                    Contemporary design
+                </li>
+                <li class="cs-li">
+                    <img class="cs-li-img" loading="lazy" decoding="async" src="media/svg/checkmark-green.svg" alt="icon" width="26" height="28" aria-hidden="true">
+                    Sturdy steel frame
+                </li>
+                <li class="cs-li">
+                    <img class="cs-li-img" loading="lazy" decoding="async" src="media/svg/checkmark-green.svg" alt="icon" width="26" height="28" aria-hidden="true">
+                    Australian made Colorbond
+                </li>
+            </ul>
+            <a class="cs-button-solid" aria-label="learn more about our programs" href="contact">Get a quote</a>
+        </div>
+    </div>
+</section>
+                                
