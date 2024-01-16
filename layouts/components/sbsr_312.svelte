060599815b7c4ca9edfc4d9c351414eb60b3f841
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-01-17 10:35:07 +1100

    add new comp sbsr 312

---
+++ layouts/components/sbsr_312.svelte
@@ -0,0 +1,39 @@
+<!-- ============================================ -->
+<!--             Side By Side Reverse 312        -->
+<!-- ============================================ -->
+
+<section id="sbsr-312">
+    <div class="cs-container">
+        <!-- Left Image Section -->
+        <div class="cs-image-group">
+            <picture class="cs-picture cs-picture1">
+                <source media="(max-width: 600px)" srcset="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/lawyers-m.jpg">
+                <source media="(min-width: 601px)" srcset="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/lawyers.jpg">
+                <img loading="lazy" decoding="async" src="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/lawyers.jpg" alt="lawyers" width="522" height="581" aria-hidden="true">
+            </picture>
+            <picture class="cs-picture cs-picture2">
+                <source media="(max-width: 600px)" srcset="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/gavel-m.jpg">
+                <source media="(min-width: 601px)" srcset="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/gavel.jpg">
+                <img loading="lazy" decoding="async" src="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/gavel.jpg" alt="gavel" width="414" height="400" aria-hidden="true">
+            </picture>
+        </div>
+        <!-- Right Content Section-->
+        <div class="cs-content">
+            <span class="cs-topper">About Us</span>
+            <h2 class="cs-title">About Inspired Fencing</h2>
+            <p class="cs-text">
+                Inspired fencing is a local family business based in Somerville on the beautiful Mornington Peninsula. At Inspired fencing, we endeavor to go that extra mile for our clients in making sure your fence is built how you want from first inspection and quoting to starting and finishing construction. If you require any further information, have any questions, or would like to arrange a time to book your free measure & quote, please feel free to contact us today.
+            </p>
+            <div class="cs-quote">
+                <span class="cs-quote-text">
+                    We take pride in our commitment to excellence, backing our work with a 100% workmanship guarantee. If you encounter any concerns or issues with any aspect of our fencing or gate installations, we are dedicated to addressing and resolving them promptly. At Inspired Fencing, our primary objective is to ensure every client is not just satisfied but 100% delighted with the outcome of our work.
+                </span>
+                <span class="cs-name">Rowan Higgs</span>
+                <span class="cs-job">Owner - Principle fencer</span>
+                <img class="cs-quote-icon" loading="lazy" decoding="async" src="media/svg/quote-marks-green.svg" alt="green quote marks" width="136" height="77">
+            </div>
+            <a href="contact" class="cs-button-solid">Get in Touch</a>
+        </div>
+    </div>
+</section>
+                                
