e91c2e181d022e2546ab4181da5a2ccf36409e35
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-01-17 10:35:42 +1100

    update text content

--- layouts/components/services_479.svelte
+++ layouts/components/services_479.svelte
@@ -6,51 +6,50 @@
     <div class="cs-container">
         <div class="cs-left-section">
             <!--Need to have in a wrapper so it's easier to arrange their position at the bottom-->
-            <a class="cs-cta" href=".">
+            <a class="cs-cta" href="testimonials">
                 <picture class="cs-arrow-wrap">
                     <img class="cs-arrow" aria-hidden="true" src="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/arrow.svg" loading="lazy" decoding="async" alt="arrow" width="70" height="70">
                 </picture>
                 <div class="cs-info">
                     <span class="cs-tag">See Our Reviews</span>
-                    <span class="cs-desc">5/5 on Google</span>
                 </div>
             </a>
             <!--Background Image-->
             <picture class="cs-background">
-                <source media="(max-width: 600px)" srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/SideBySide/tall-grass.jpg">
-                <source media="(min-width: 601px)" srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/SideBySide/tall-grass.jpg">
-                <img aria-hidden="true" loading="lazy" decoding="async" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/SideBySide/tall-grass.jpg" alt="grass" width="625" height="842">
+                <source media="(max-width: 600px)" srcset="media/images/Paling.jpeg">
+                <source media="(min-width: 601px)" srcset="media/images/Paling.jpeg">
+                <img aria-hidden="true" loading="lazy" decoding="async" src="media/images/Paling.jpeg" alt="grass" width="625" height="842">
             </picture>
         </div>
         <!--We need to wrap these two in a wrapper so they can be positioned more accurately and easily -->
         <div class="cs-right-section">
             <div class="cs-content">
-                <span class="cs-topper">Features</span>
-                <h2 class="cs-title">Side by Side Section Example Right </h2>
+                <span class="cs-topper">Fencing Services</span>
+                <h2 class="cs-title">Trusted Fencing Contractor in Flinders</h2>
                 <p class="cs-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Our unwavering dedication to customer satisfaction sets us apart. If you're in search of a fencing and gate contractor that gets the job done right from the start, your search ends here. Choose Inspired Fencing for a seamless blend of quality, reliability, and customer-centric service that leaves you with security and style that exceeds your expectations."
                 </p>
             </div>
             <ul class="cs-card-group">
                 <li class="cs-item">
                     <picture class="cs-icon-wrap">
-                        <img class="cs-icon" aria-hidden="true" src="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/geen1.svg" loading="lazy" decoding="async" alt="moon" width="30" height="30">
+                        <img class="cs-icon" aria-hidden="true" src="media/svg/picket-fence-svgrepo-com.svg" loading="lazy" decoding="async" alt="Fence icon" width="30" height="30">
                     </picture>
-                    <h3 class="cs-h3">Service 1</h3>
+                    <h3 class="cs-h3">Fences</h3>
                     <p class="cs-item-text">
                         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, sapiente.
                     </p>
-                    <a href="." class="cs-link">Read More</a>
+                    <a href="services" class="cs-link">Read More</a>
                 </li>
                 <li class="cs-item">
                     <picture class="cs-icon-wrap">
-                        <img class="cs-icon" aria-hidden="true" src="https://csimg.nyc3.digitaloceanspaces.com/SideBySide/geen1.svg" loading="lazy" decoding="async" alt="moon" width="30" height="30">
+                        <img class="cs-icon" aria-hidden="true" src="media/svg/gate-svgrepo-com.svg" loading="lazy" decoding="async" alt="Gate icon" width="30" height="30">
                     </picture>
-                    <h3 class="cs-h3">Service 1</h3>
+                    <h3 class="cs-h3">Gates</h3>
                     <p class="cs-item-text">
                         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, sapiente.
                     </p>
-                    <a href="." class="cs-link">Read More</a>
+                    <a href="services" class="cs-link">Read More</a>
                 </li>
             </ul>
         </div>
