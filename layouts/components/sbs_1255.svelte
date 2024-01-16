bee849cd69f4d60cad1cca6abe2cf6b29bdbfd3c
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-01-17 10:34:38 +1100

    add new comp sbs 1255

---
+++ layouts/components/sbs_1255.svelte
@@ -0,0 +1,84 @@
+<!-- ============================================ -->
+<!--                Side By Side                  -->
+<!-- ============================================ -->
+
+<section id="sbs-1255">
+    <div class="cs-container">
+        <div class="cs-content">
+            <span class="cs-topper">Colorbond</span>
+            <h2 class="cs-title">Delivering high-quality workmanship</h2>
+            <p class="cs-text">
+                We received a call from an existing client in November 2018. He had a car drive through his rental property fence destroying a large section. After having previously completed a Colorbond fence between this property and the neighboring units for this client back in June, I was pleased to receive his call to help him with a replacement Colorbond fence, despite the unfortunate circumstances. The client also had a feature gate that was damaged. This gate was a great challenge as we had not done one quite like it before, but we (and the client) are pleased with the outcome.
+            </p>
+            <p class="cs-text"><i>Features of Colorbond:</i></p>
+            <ul class="cs-ul">
+                <li class="cs-li">
+                    <img class="cs-li-img" loading="lazy" decoding="async" src="media/svg/checkmark-green.svg" alt="icon" width="26" height="28" aria-hidden="true">
+                    Enclose your private space
+                </li>
+                <li class="cs-li">
+                    <img class="cs-li-img" loading="lazy" decoding="async" src="media/svg/checkmark-green.svg" alt="icon" width="26" height="28" aria-hidden="true">
+                    Contemporary design
+                </li>
+                <li class="cs-li">
+                    <img class="cs-li-img" loading="lazy" decoding="async" src="media/svg/checkmark-green.svg" alt="icon" width="26" height="28" aria-hidden="true">
+                    Maintenance-free
+                </li>
+                <li class="cs-li">
+                    <img class="cs-li-img" loading="lazy" decoding="async" src="media/svg/checkmark-green.svg" alt="icon" width="26" height="28" aria-hidden="true">
+                    Australian made
+                </li>
+            </ul>
+            <a href="contact" class="cs-button-solid">Get a quote</a>
+        </div>
+        <div class="cs-image-group">
+            <picture class="cs-picture cs-picture1">
+                <!--Mobile Image-->
+                <source media="(max-width: 600px)" srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/MISC/building1.jpg">
+                <!--Tablet and above Image-->
+                <source media="(min-width: 601px)" srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/MISC/building1.jpg">
+                <img loading="lazy" decoding="async" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/MISC/building1.jpg" alt="building" width="203" height="203">
+            </picture>
+            <picture class="cs-picture cs-picture2">
+                <!--Mobile Image-->
+                <source media="(max-width: 600px)" srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/MISC/building2.jpg">
+                <!--Tablet and above Image-->
+                <source media="(min-width: 601px)" srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/MISC/building2.jpg">
+                <img loading="lazy" decoding="async" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/MISC/building2.jpg" alt="building" width="207" height="207">
+            </picture>
+            <picture class="cs-picture cs-picture3">
+                <!--Mobile Image-->
+                <source media="(max-width: 600px)" srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/MISC/building3.jpg">
+                <!--Tablet and above Image-->
+                <source media="(min-width: 601px)" srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/MISC/building3.jpg">
+                <img loading="lazy" decoding="async" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/MISC/building3.jpg" alt="building" width="214" height="214">
+            </picture>
+            <picture class="cs-picture cs-picture4">
+                <!--Mobile Image-->
+                <source media="(max-width: 600px)" srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/MISC/building4.jpg">
+                <!--Tablet and above Image-->
+                <source media="(min-width: 601px)" srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/MISC/building4.jpg">
+                <img loading="lazy" decoding="async" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/MISC/building4.jpg" alt="building" width="223" height="223">
+            </picture>
+            <picture class="cs-picture cs-picture5">
+                <!--Mobile Image-->
+                <source media="(max-width: 600px)" srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/MISC/building5.jpg">
+                <!--Tablet and above Image-->
+                <source media="(min-width: 601px)" srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/MISC/building5.jpg">
+                <img loading="lazy" decoding="async" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/MISC/building5.jpg" alt="building" width="287" height="287">
+            </picture>
+            <picture class="cs-picture cs-picture6">
+                <!--Mobile Image-->
+                <source media="(max-width: 600px)" srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/MISC/building6.jpg">
+                <!--Tablet and above Image-->
+                <source media="(min-width: 601px)" srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/MISC/building6.jpg">
+                <img loading="lazy" decoding="async" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/MISC/building6.jpg" alt="building" width="203" height="203">
+            </picture>
+            <!--Stripe background-->
+            <picture class="cs-waves-wrapper">
+                <img class="cs-waves" loading="lazy" decoding="async" src="media/svg/svg-waves-green.svg" alt="stripes" width="650" height="933" aria-hidden="true">
+            </picture>
+        </div>
+    </div>
+</section>
+                                
