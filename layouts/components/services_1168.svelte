632cb560ae296d7354927cf0d591eb0e87745357
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-01-17 10:36:03 +1100

    add new comp services 1168

---
+++ layouts/components/services_1168.svelte
@@ -0,0 +1,322 @@
+<!-- ============================================ -->
+<!--                  Services                    -->
+<!-- ============================================ -->
+
+<section id="services-1168">
+    <div class="cs-container">
+      <div class="cs-content">
+        <span class="cs-topper">Our Service</span>
+        <h2 class="cs-title">Our Range of Products</h2>
+        <p class="cs-text">
+            At Inspired Fencing, we focus on offering quality products and services tailored to the needs and budgets of our customers. We accept custom projects as well.Whether you are in need of gates around your property for your security, a new fence for your house or a fence for surrounding your pool, you can rest assure that the team at Inspired Fencing will be able to provide you with a quality solution in timely manner. We consider no job to be too small or too big. See our range of fencing styles below.
+        </p>
+      </div>
+      <ul class="cs-card-group">
+        <li class="cs-item">
+          <picture class="cs-picture">
+            <!--Mobile Image-->
+            <source
+                  media="(max-width: 600px)"
+                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic30.jpg">
+            <!--Tablet and above Image-->
+            <source
+                  media="(min-width: 601px)"
+                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic30.jpg">
+            <img
+                  loading="lazy"
+                  decoding="async"
+                  src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic30.jpg"
+                  alt="mechanic"
+                  width="413"
+                  height="240"
+                  aria-hidden="true">
+          </picture>
+          <div class="cs-info">
+            <h3 class="cs-h3">Colorbond</h3>
+            <p class="cs-item-text">
+              What is Check Engine Light? There are many variations of passages of Lorem Ipsum available, but the majority have...
+            </p>
+<!--             <a href="." class="cs-link">
+              Read More
+              <img
+                    class="cs-arrow"
+                    loading="lazy"
+                    decoding="async"
+                    src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/red-arrow-right.svg"
+                    alt="icon"
+                    width="20"
+                    height="20"
+                    aria-hidden="true">
+            </a> -->
+          </div>
+        </li>
+        <li class="cs-item">
+          <picture class="cs-picture">
+            <!--Mobile Image-->
+            <source
+                  media="(max-width: 600px)"
+                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic33.jpg">
+            <!--Tablet and above Image-->
+            <source
+                  media="(min-width: 601px)"
+                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic33.jpg">
+            <img
+                  loading="lazy"
+                  decoding="async"
+                  src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic33.jpg"
+                  alt="mechanic"
+                  width="413"
+                  height="240"
+                  aria-hidden="true">
+          </picture>
+          <div class="cs-info">
+            <h3 class="cs-h3">Timber</h3>
+            <p class="cs-item-text">
+              Our featured services Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur...
+            </p>
+<!--             <a href="." class="cs-link">
+              Read More
+              <img
+                    class="cs-arrow"
+                    loading="lazy"
+                    decoding="async"
+                    src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/red-arrow-right.svg"
+                    alt="icon"
+                    width="20"
+                    height="20"
+                    aria-hidden="true">
+            </a> -->
+          </div>
+        </li>
+        <li class="cs-item">
+          <picture class="cs-picture">
+            <!--Mobile Image-->
+            <source
+                  media="(max-width: 600px)"
+                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic32.jpg">
+            <!--Tablet and above Image-->
+            <source
+                  media="(min-width: 601px)"
+                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic32.jpg">
+            <img
+                  loading="lazy"
+                  decoding="async"
+                  src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic32.jpg"
+                  alt="mechanic"
+                  width="413"
+                  height="240"
+                  aria-hidden="true">
+          </picture>
+          <div class="cs-info">
+            <h3 class="cs-h3">Security</h3>
+            <p class="cs-item-text">
+              Our services Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni...
+            </p>
+<!--             <a href="." class="cs-link">
+              Read More
+              <img
+                    class="cs-arrow"
+                    loading="lazy"
+                    decoding="async"
+                    src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/red-arrow-right.svg"
+                    alt="icon"
+                    width="20"
+                    height="20"
+                    aria-hidden="true">
+            </a> -->
+          </div>
+        </li>
+        <li class="cs-item">
+          <picture class="cs-picture">
+            <!--Mobile Image-->
+            <source
+                  media="(max-width: 600px)"
+                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic31.jpg">
+            <!--Tablet and above Image-->
+            <source
+                  media="(min-width: 601px)"
+                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic31.jpg">
+            <img
+                  loading="lazy"
+                  decoding="async"
+                  src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic31.jpg"
+                  alt="mechanic"
+                  width="413"
+                  height="240"
+                  aria-hidden="true">
+          </picture>
+          <div class="cs-info">
+            <h3 class="cs-h3">Tubular</h3>
+            <p class="cs-item-text">
+              What is Check Engine Light? There are many variations of passages of Lorem Ipsum available, but the majority have...
+            </p>
+<!--             <a href="." class="cs-link">
+              Read More
+              <img
+                    class="cs-arrow"
+                    loading="lazy"
+                    decoding="async"
+                    src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/red-arrow-right.svg"
+                    alt="icon"
+                    width="20"
+                    height="20"
+                    aria-hidden="true">
+            </a> -->
+          </div>
+        </li>
+        <li class="cs-item">
+          <picture class="cs-picture">
+            <!--Mobile Image-->
+            <source
+                  media="(max-width: 600px)"
+                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic30.jpg">
+            <!--Tablet and above Image-->
+            <source
+                  media="(min-width: 601px)"
+                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic30.jpg">
+            <img
+                  loading="lazy"
+                  decoding="async"
+                  src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic30.jpg"
+                  alt="mechanic"
+                  width="413"
+                  height="240"
+                  aria-hidden="true">
+          </picture>
+          <div class="cs-info">
+            <h3 class="cs-h3">Chainwire</h3>
+            <p class="cs-item-text">
+              What is Check Engine Light? There are many variations of passages of Lorem Ipsum available, but the majority have...
+            </p>
+<!--             <a href="." class="cs-link">
+              Read More
+              <img
+                    class="cs-arrow"
+                    loading="lazy"
+                    decoding="async"
+                    src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/red-arrow-right.svg"
+                    alt="icon"
+                    width="20"
+                    height="20"
+                    aria-hidden="true">
+            </a> -->
+          </div>
+        </li>
+        <li class="cs-item">
+          <picture class="cs-picture">
+            <!--Mobile Image-->
+            <source
+                  media="(max-width: 600px)"
+                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic33.jpg">
+            <!--Tablet and above Image-->
+            <source
+                  media="(min-width: 601px)"
+                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic33.jpg">
+            <img
+                  loading="lazy"
+                  decoding="async"
+                  src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic33.jpg"
+                  alt="mechanic"
+                  width="413"
+                  height="240"
+                  aria-hidden="true">
+          </picture>
+          <div class="cs-info">
+            <h3 class="cs-h3">Brush</h3>
+            <p class="cs-item-text">
+              Our featured services Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur...
+            </p>
+<!--             <a href="." class="cs-link">
+              Read More
+              <img
+                    class="cs-arrow"
+                    loading="lazy"
+                    decoding="async"
+                    src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/red-arrow-right.svg"
+                    alt="icon"
+                    width="20"
+                    height="20"
+                    aria-hidden="true">
+            </a> -->
+          </div>
+        </li>
+        <li class="cs-item">
+          <picture class="cs-picture">
+            <!--Mobile Image-->
+            <source
+                  media="(max-width: 600px)"
+                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic32.jpg">
+            <!--Tablet and above Image-->
+            <source
+                  media="(min-width: 601px)"
+                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic32.jpg">
+            <img
+                  loading="lazy"
+                  decoding="async"
+                  src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic32.jpg"
+                  alt="mechanic"
+                  width="413"
+                  height="240"
+                  aria-hidden="true">
+          </picture>
+          <div class="cs-info">
+            <h3 class="cs-h3">LouvrLat</h3>
+            <p class="cs-item-text">
+              Our services Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni...
+            </p>
+<!--             <a href="." class="cs-link">
+              Read More
+              <img
+                    class="cs-arrow"
+                    loading="lazy"
+                    decoding="async"
+                    src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/red-arrow-right.svg"
+                    alt="icon"
+                    width="20"
+                    height="20"
+                    aria-hidden="true">
+            </a> -->
+          </div>
+        </li>
+        <li class="cs-item">
+          <picture class="cs-picture">
+            <!--Mobile Image-->
+            <source
+                  media="(max-width: 600px)"
+                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic31.jpg">
+            <!--Tablet and above Image-->
+            <source
+                  media="(min-width: 601px)"
+                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic31.jpg">
+            <img
+                  loading="lazy"
+                  decoding="async"
+                  src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic31.jpg"
+                  alt="mechanic"
+                  width="413"
+                  height="240"
+                  aria-hidden="true">
+          </picture>
+          <div class="cs-info">
+            <h3 class="cs-h3">Featured</h3>
+            <p class="cs-item-text">
+              What is Check Engine Light? There are many variations of passages of Lorem Ipsum available, but the majority have...
+            </p>
+           <!--  <a href="." class="cs-link">
+              Read More
+              <img
+                    class="cs-arrow"
+                    loading="lazy"
+                    decoding="async"
+                    src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/red-arrow-right.svg"
+                    alt="icon"
+                    width="20"
+                    height="20"
+                    aria-hidden="true">
+            </a> -->
+          </div>
+        </li>
+      </ul>
+      <a href="contact" class="cs-button-solid">Ask for a Quote</a>
+    </div>
+</section>
