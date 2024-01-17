041d32bfe8732f12259fd4f9c4a82d17a9321fc4
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-01-17 15:07:46 +1100

    update image links and text

--- layouts/components/cta_403.svelte
+++ layouts/components/cta_403.svelte
@@ -5,19 +5,19 @@
 <section id="cta-403">
     <div class="cs-container">
         <div class="cs-content">
-            <span class="cs-topper">Our Portfolio</span>
+            <span class="cs-topper">Inspired Fencing</span>
             <h2 class="cs-title">Want to Work with Our Team?</h2>
             <p class="cs-text">
-                We love what we do and we do it with passion. We value the experimentation of the message and smart incentives.
+                We love what we do and we do it with passion. We value quality workmanship and customer satisfaction.
             </p>
-            <a href="." class="cs-button-solid">Read More</a>
+            <a href="services" class="cs-button-solid">See our Services</a>
         </div>
     </div>
     <!--Background Image-->
     <picture class="cs-background">
-        <source media="(max-width: 600px)" srcset="https://csimg.nyc3.digitaloceanspaces.com/CTA/cleaning2-m.jpg">
-        <source media="(min-width: 601px)" srcset="https://csimg.nyc3.digitaloceanspaces.com/CTA/cleaning2.jpg">
-        <img loading="lazy" decoding="async" src="https://csimg.nyc3.digitaloceanspaces.com/CTA/cleaning2.jpg" alt="cleaner" width="1920" height="660" aria-hidden="true">
+        <source media="(max-width: 600px)" srcset="media/images/Feature_paling.jpeg">
+        <source media="(min-width: 601px)" srcset="media/images/Feature_paling.jpeg">
+        <img loading="lazy" decoding="async" src="media/images/Feature_paling.jpeg" alt="cleaner" width="1920" height="660" aria-hidden="true">
     </picture>
 </section>
                                 
