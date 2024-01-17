ef1bb24b3b74450aa572a7b4bdce48d0d7f164b9
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-01-17 15:08:16 +1100

    add icons links and update text

--- layouts/components/reviews_607.svelte
+++ layouts/components/reviews_607.svelte
@@ -6,14 +6,14 @@
     <div class="cs-container">
         <div class="cs-content">
             <span class="cs-topper">Testimonials</span>
-            <h2 class="cs-title">Trusted by Thousand of People & Companies</h2>
+            <h2 class="cs-title">Trusted by Thousand of home oweners & Companies</h2>
             <p class="cs-text">
-                Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.
+                We don't just boast about our work; our clients place their trust in us. See for yourself what a few of our customers have shared about their interactions with Inspired Fencing.
             </p>
         </div>
         <ul class="cs-card-group">
             <li class="cs-item">
-                <img class="cs-icon" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Reviews/yellow-quote.svg" alt="quote icon" aria-hidden="true" width="40" height="38" loading="lazy" decoding="async">
+                <img class="cs-icon" src="media/svg/green-quote-marks.svg" alt="quote icon" aria-hidden="true" width="40" height="38" loading="lazy" decoding="async">
                 <h3 class="cs-h3">Loving the color!</h3>
                 <p class="cs-item-text">
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores iste vitae, aliquam temporibus modi libero dolorem rerum? Dolore soluta eligendi mollitia. Doloribus et exercitationem cupiditate?
@@ -21,7 +21,7 @@
                 <span class="cs-name">Daniel</span>
             </li>
             <li class="cs-item">
-                <img class="cs-icon" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Reviews/yellow-quote.svg" alt="quote icon" aria-hidden="true" width="40" height="38" loading="lazy" decoding="async">
+                <img class="cs-icon" src="media/svg/green-quote-marks.svg" alt="quote icon" aria-hidden="true" width="40" height="38" loading="lazy" decoding="async">
                 <h3 class="cs-h3">Affordable</h3>
                 <p class="cs-item-text">
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores iste vitae, aliquam temporibus modi libero dolorem rerum? Dolore soluta eligendi mollitia. Doloribus et exercitationem cupiditate?
@@ -29,7 +29,7 @@
                 <span class="cs-name">Daniel</span>
             </li>
             <li class="cs-item">
-                <img class="cs-icon" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Reviews/yellow-quote.svg" alt="quote icon" aria-hidden="true" width="40" height="38" loading="lazy" decoding="async">
+                <img class="cs-icon" src="media/svg/green-quote-marks.svg" alt="quote icon" aria-hidden="true" width="40" height="38" loading="lazy" decoding="async">
                 <h3 class="cs-h3">Professional and the best!</h3>
                 <p class="cs-item-text">
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores iste vitae, aliquam temporibus modi libero dolorem rerum? Dolore soluta eligendi mollitia. Doloribus et exercitationem cupiditate?
