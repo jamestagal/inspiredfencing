c66b82dade808ebeec91cfdc376a8082cedf2519
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-01-17 15:10:43 +1100

    add replacement pattern

--- layouts/components/services_1168.svelte
+++ layouts/components/services_1168.svelte
@@ -1,3 +1,6 @@
+<script>
+  export let subtitle, title, description, card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11, card12, button;
+</script>
 <!-- ============================================ -->
 <!--                  Services                    -->
 <!-- ============================================ -->
@@ -5,10 +8,10 @@
 <section id="services-1168">
     <div class="cs-container">
       <div class="cs-content">
-        <span class="cs-topper">Our Service</span>
-        <h2 class="cs-title">Our Range of Products</h2>
+        <span class="cs-topper">{subtitle}</span>
+        <h2 class="cs-title">{title}</h2>
         <p class="cs-text">
-            At Inspired Fencing, we focus on offering quality products and services tailored to the needs and budgets of our customers. We accept custom projects as well.Whether you are in need of gates around your property for your security, a new fence for your house or a fence for surrounding your pool, you can rest assure that the team at Inspired Fencing will be able to provide you with a quality solution in timely manner. We consider no job to be too small or too big. See our range of fencing styles below.
+            {description}
         </p>
       </div>
       <ul class="cs-card-group">
@@ -17,24 +20,24 @@
             <!--Mobile Image-->
             <source
                   media="(max-width: 600px)"
-                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic30.jpg">
+                  srcset="{card1.image_m.url}">
             <!--Tablet and above Image-->
             <source
                   media="(min-width: 601px)"
-                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic30.jpg">
+                  srcset="{card1.image_t.url}">
             <img
                   loading="lazy"
                   decoding="async"
-                  src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic30.jpg"
-                  alt="mechanic"
+                  src="{card1.image.url}"
+                  alt="{card1.image.alt}"
                   width="413"
                   height="240"
                   aria-hidden="true">
           </picture>
           <div class="cs-info">
-            <h3 class="cs-h3">Colorbond</h3>
+            <h3 class="cs-h3">{card1.title}</h3>
             <p class="cs-item-text">
-              What is Check Engine Light? There are many variations of passages of Lorem Ipsum available, but the majority have...
+              {card1.description}.
             </p>
 <!--             <a href="." class="cs-link">
               Read More
@@ -55,24 +58,24 @@
             <!--Mobile Image-->
             <source
                   media="(max-width: 600px)"
-                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic33.jpg">
+                  srcset="{card2.image_m.url}">
             <!--Tablet and above Image-->
             <source
                   media="(min-width: 601px)"
-                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic33.jpg">
+                  srcset="{card2.image_t.url}">
             <img
                   loading="lazy"
                   decoding="async"
-                  src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic33.jpg"
-                  alt="mechanic"
+                  src="{card2.image.url}"
+                  alt="{card2.image.alt}"
                   width="413"
                   height="240"
                   aria-hidden="true">
           </picture>
           <div class="cs-info">
-            <h3 class="cs-h3">Timber</h3>
+            <h3 class="cs-h3">{card2.title}</h3>
             <p class="cs-item-text">
-              Our featured services Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur...
+              {card2.description}.
             </p>
 <!--             <a href="." class="cs-link">
               Read More
@@ -93,24 +96,24 @@
             <!--Mobile Image-->
             <source
                   media="(max-width: 600px)"
-                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic32.jpg">
+                  srcset="{card3.image_m.url}">
             <!--Tablet and above Image-->
             <source
                   media="(min-width: 601px)"
-                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic32.jpg">
+                  srcset="{card3.image_t.url}">
             <img
                   loading="lazy"
                   decoding="async"
-                  src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic32.jpg"
-                  alt="mechanic"
+                  src="{card3.image.url}"
+                  alt="{card3.image.alt}"
                   width="413"
                   height="240"
                   aria-hidden="true">
           </picture>
           <div class="cs-info">
-            <h3 class="cs-h3">Security</h3>
+            <h3 class="cs-h3">{card3.title}</h3>
             <p class="cs-item-text">
-              Our services Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni...
+              {card3.description}.
             </p>
 <!--             <a href="." class="cs-link">
               Read More
@@ -131,24 +134,24 @@
             <!--Mobile Image-->
             <source
                   media="(max-width: 600px)"
-                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic31.jpg">
+                  srcset="{card4.image_m.url}">
             <!--Tablet and above Image-->
             <source
                   media="(min-width: 601px)"
-                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic31.jpg">
+                  srcset="{card4.image_t.url}">
             <img
                   loading="lazy"
                   decoding="async"
-                  src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic31.jpg"
-                  alt="mechanic"
+                  src="{card4.image.url}"
+                  alt="{card4.image.alt}"
                   width="413"
                   height="240"
                   aria-hidden="true">
           </picture>
           <div class="cs-info">
-            <h3 class="cs-h3">Tubular</h3>
+            <h3 class="cs-h3">{card4.title}</h3>
             <p class="cs-item-text">
-              What is Check Engine Light? There are many variations of passages of Lorem Ipsum available, but the majority have...
+              {card4.description}.
             </p>
 <!--             <a href="." class="cs-link">
               Read More
@@ -169,24 +172,24 @@
             <!--Mobile Image-->
             <source
                   media="(max-width: 600px)"
-                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic30.jpg">
+                  srcset="{card5.image_m.url}">
             <!--Tablet and above Image-->
             <source
                   media="(min-width: 601px)"
-                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic30.jpg">
+                  srcset="{card5.image_t.url}">
             <img
                   loading="lazy"
                   decoding="async"
-                  src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic30.jpg"
-                  alt="mechanic"
+                  src="{card5.image.url}"
+                  alt="{card5.image.alt}"
                   width="413"
                   height="240"
                   aria-hidden="true">
           </picture>
           <div class="cs-info">
-            <h3 class="cs-h3">Chainwire</h3>
+            <h3 class="cs-h3">{card5.title}</h3>
             <p class="cs-item-text">
-              What is Check Engine Light? There are many variations of passages of Lorem Ipsum available, but the majority have...
+              {card5.description}.
             </p>
 <!--             <a href="." class="cs-link">
               Read More
@@ -207,24 +210,24 @@
             <!--Mobile Image-->
             <source
                   media="(max-width: 600px)"
-                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic33.jpg">
+                  srcset="{card6.image_m.url}">
             <!--Tablet and above Image-->
             <source
                   media="(min-width: 601px)"
-                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic33.jpg">
+                  srcset="{card6.image_t.url}">
             <img
                   loading="lazy"
                   decoding="async"
-                  src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic33.jpg"
-                  alt="mechanic"
+                  src="{card6.image.url}"
+                  alt="{card6.image.alt}"
                   width="413"
                   height="240"
                   aria-hidden="true">
           </picture>
           <div class="cs-info">
-            <h3 class="cs-h3">Brush</h3>
+            <h3 class="cs-h3">{card6.title}</h3>
             <p class="cs-item-text">
-              Our featured services Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur...
+              {card6.description}.
             </p>
 <!--             <a href="." class="cs-link">
               Read More
@@ -245,24 +248,24 @@
             <!--Mobile Image-->
             <source
                   media="(max-width: 600px)"
-                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic32.jpg">
+                  srcset="{card7.image_m.url}">
             <!--Tablet and above Image-->
             <source
                   media="(min-width: 601px)"
-                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic32.jpg">
+                  srcset="{card7.image_t.url}">
             <img
                   loading="lazy"
                   decoding="async"
-                  src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic32.jpg"
-                  alt="mechanic"
+                  src="{card7.image.url}"
+                  alt="{card7.image.alt}"
                   width="413"
                   height="240"
                   aria-hidden="true">
           </picture>
           <div class="cs-info">
-            <h3 class="cs-h3">LouvrLat</h3>
+            <h3 class="cs-h3">{card7.title}</h3>
             <p class="cs-item-text">
-              Our services Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni...
+              {card7.description}.
             </p>
 <!--             <a href="." class="cs-link">
               Read More
@@ -283,26 +286,178 @@
             <!--Mobile Image-->
             <source
                   media="(max-width: 600px)"
-                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic31.jpg">
+                  srcset="{card8.image_m.url}">
             <!--Tablet and above Image-->
             <source
                   media="(min-width: 601px)"
-                  srcset="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic31.jpg">
+                  srcset="{card8.image_t.url}">
             <img
                   loading="lazy"
                   decoding="async"
-                  src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic31.jpg"
-                  alt="mechanic"
+                  src="{card8.image.url}"
+                  alt="{card8.image.alt}"
                   width="413"
                   height="240"
                   aria-hidden="true">
           </picture>
           <div class="cs-info">
-            <h3 class="cs-h3">Featured</h3>
+            <h3 class="cs-h3">{card8.title}</h3>
             <p class="cs-item-text">
-              What is Check Engine Light? There are many variations of passages of Lorem Ipsum available, but the majority have...
+              {card8.description}.
             </p>
-           <!--  <a href="." class="cs-link">
+<!--             <a href="." class="cs-link">
+              Read More
+              <img
+                    class="cs-arrow"
+                    loading="lazy"
+                    decoding="async"
+                    src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/red-arrow-right.svg"
+                    alt="icon"
+                    width="20"
+                    height="20"
+                    aria-hidden="true">
+            </a> -->
+          </div>
+        </li>
+        <li class="cs-item">
+          <picture class="cs-picture">
+            <!--Mobile Image-->
+            <source
+                  media="(max-width: 600px)"
+                  srcset="{card9.image_m.url}">
+            <!--Tablet and above Image-->
+            <source
+                  media="(min-width: 601px)"
+                  srcset="{card9.image_t.url}">
+            <img
+                  loading="lazy"
+                  decoding="async"
+                  src="{card9.image.url}"
+                  alt="{card9.image.alt}"
+                  width="413"
+                  height="240"
+                  aria-hidden="true">
+          </picture>
+          <div class="cs-info">
+            <h3 class="cs-h3">{card9.title}</h3>
+            <p class="cs-item-text">
+              {card9.description}.
+            </p>
+<!--             <a href="." class="cs-link">
+              Read More
+              <img
+                    class="cs-arrow"
+                    loading="lazy"
+                    decoding="async"
+                    src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/red-arrow-right.svg"
+                    alt="icon"
+                    width="20"
+                    height="20"
+                    aria-hidden="true">
+            </a> -->
+          </div>
+        </li>
+        <li class="cs-item">
+          <picture class="cs-picture">
+            <!--Mobile Image-->
+            <source
+                  media="(max-width: 600px)"
+                  srcset="{card10.image_m.url}">
+            <!--Tablet and above Image-->
+            <source
+                  media="(min-width: 601px)"
+                  srcset="{card10.image_t.url}">
+            <img
+                  loading="lazy"
+                  decoding="async"
+                  src="{card10.image.url}"
+                  alt="{card10.image.alt}"
+                  width="413"
+                  height="240"
+                  aria-hidden="true">
+          </picture>
+          <div class="cs-info">
+            <h3 class="cs-h3">{card10.title}</h3>
+            <p class="cs-item-text">
+              {card10.description}.
+            </p>
+<!--             <a href="." class="cs-link">
+              Read More
+              <img
+                    class="cs-arrow"
+                    loading="lazy"
+                    decoding="async"
+                    src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/red-arrow-right.svg"
+                    alt="icon"
+                    width="20"
+                    height="20"
+                    aria-hidden="true">
+            </a> -->
+          </div>
+        </li>
+        <li class="cs-item">
+          <picture class="cs-picture">
+            <!--Mobile Image-->
+            <source
+                  media="(max-width: 600px)"
+                  srcset="{card11.image_m.url}">
+            <!--Tablet and above Image-->
+            <source
+                  media="(min-width: 601px)"
+                  srcset="{card11.image_t.url}">
+            <img
+                  loading="lazy"
+                  decoding="async"
+                  src="{card11.image.url}"
+                  alt="{card11.image.alt}"
+                  width="413"
+                  height="240"
+                  aria-hidden="true">
+          </picture>
+          <div class="cs-info">
+            <h3 class="cs-h3">{card11.title}</h3>
+            <p class="cs-item-text">
+              {card11.description}.
+            </p>
+<!--             <a href="." class="cs-link">
+              Read More
+              <img
+                    class="cs-arrow"
+                    loading="lazy"
+                    decoding="async"
+                    src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/red-arrow-right.svg"
+                    alt="icon"
+                    width="20"
+                    height="20"
+                    aria-hidden="true">
+            </a> -->
+          </div>
+        </li>
+        <li class="cs-item">
+          <picture class="cs-picture">
+            <!--Mobile Image-->
+            <source
+                  media="(max-width: 600px)"
+                  srcset="{card12.image_m.url}">
+            <!--Tablet and above Image-->
+            <source
+                  media="(min-width: 601px)"
+                  srcset="{card12.image_t.url}">
+            <img
+                  loading="lazy"
+                  decoding="async"
+                  src="{card12.image.url}"
+                  alt="{card12.image.alt}"
+                  width="413"
+                  height="240"
+                  aria-hidden="true">
+          </picture>
+          <div class="cs-info">
+            <h3 class="cs-h3">{card12.title}</h3>
+            <p class="cs-item-text">
+              {card12.description}.
+            </p>
+<!--             <a href="." class="cs-link">
               Read More
               <img
                     class="cs-arrow"
@@ -317,6 +472,6 @@
           </div>
         </li>
       </ul>
-      <a href="contact" class="cs-button-solid">Ask for a Quote</a>
+      <a href="{button.url}" class="cs-button-solid">{button.title}</a>
     </div>
 </section>
