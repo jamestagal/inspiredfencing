ddf1f75a9236010289276a6b4987f23e218f6528
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-01-17 16:05:04 +1100

    add replacement pattern

--- layouts/components/content_713.svelte
+++ layouts/components/content_713.svelte
@@ -1,3 +1,6 @@
+<script>
+    export let title1, title2, description, listTitle, list1, list2, list3, image_m, image_t, image, waves, button;
+</script>
 <!-- ============================================ -->
 <!--                  Content Pge                 -->
 <!-- ============================================ -->
@@ -5,37 +8,38 @@
 <section id="content-page-713">
     <div class="cs-container">
         <div class="cs-content">
-            <h1 class="cs-title">Fences and Gates by <br> <span style="color: var(--primary)">Inspired Fencing</span>
+            <h1 class="cs-title">{title1} <br> <span style="color: var(--primary)">{title2}</span>
             </h1>
+            {#each description as d}
             <p>
-                As Melbourne homeowners seek superior fencing solutions, Inspired Fencing emerges as the trusted choice. Offering an extensive range of materials, styles, and colors, we provide tailored residential fencing solutions that align with diverse landscapes.
+                {@html d.paragraph1}
+                <br><br>
+                {@html d.paragraph2}
             </p>
-            <p>
-                Residential fencing plays multifaceted roles, contributing to privacy, home security, defining property boundaries, and ensuring the containment of pets and small children within the backyard. Recognizing the importance of aesthetics, Inspired Fencing focuses on selecting attractive options that seamlessly blend style with substance. Expect the best in terms of durability and functionality across our entire home fencing range.
-            </p>
-            <i>Key Considerations When Choosing Your Home Fencing:</i><br>
+            {/each}
+            <i>{listTitle}</i><br>
             <ol>
-                <li><strong>Add value with attractive fences:</strong><br>
-                <p>Fences that complement your home's style significantly enhance property value. Inspired Fencing understands the importance of cohesive design, offering fencing options that perfectly tie together different architectural styles, from federation homes to contemporary houses.</p></li>
-                <li><strong>Make good first impressions with attractive fences:</strong>
-                    <p>A visually appealing fence enhances the street view of your home, contributing to its curb appeal. Inspired Fencing believes in creating positive first impressions, ensuring that your property makes a lasting impact on visitors</p></li>
-                <li><strong>Create harmony with Backyard and Garden::</strong><br>
-                    <p>For those with a green thumb, Inspired Fencing provides fences that seamlessly blend with gardens. Consider options like aluminum fencing, ideal for incorporating climbing plants, or the popular choice of glass pool fencing for an invisible and harmonious integration with your landscaped pool area.</p></li>
+                <li><strong>{list1.title}</strong><br>
+                <p>{list1.description}</p></li>
+                <li><strong>{list2.title}</strong>
+                    <p>{list2.description}</p></li>
+                <li><strong>{list3.title}</strong><br>
+                    <p>{list3.description}</p></li>
             </ol>
-            <a href="contact" class="cs-button-solid">Get An Quote</a>
+            <a href="{button.url}" class="cs-button-solid">{button.title}</a>
         </div>
         <div class="cs-image-group">
             <picture class="cs-picture">
                 <!--Mobile Image-->
-                <source media="(max-width: 600px)" srcset="media/images/fences/Colorbond_01.jpeg">
+                <source media="(max-width: 600px)" srcset="{image_m.url}">
                 <!--Tablet and above Image-->
-                <source media="(min-width: 601px)" srcset="media/images/fences/Colorbond_01.jpeg">
-                <img loading="lazy" decoding="async" src="media/images/fences/Colorbond_01.jpeg" alt="kitchen painting" width="570" height="680" aria-hidden="true">
+                <source media="(min-width: 601px)" srcset="{image_t.url}">
+                <img loading="lazy" decoding="async" src="{image.url}" alt="{image.alt}" width="570" height="680" aria-hidden="true">
             </picture>
         </div>
     </div>
     <!--Waves Graphic-->
     <picture class="cs-background">
-        <img src="https://d33wubrfki0l68.cloudfront.net/a585693f05dd2347d32aec7438140fcc3f6dc3fc/723a2/assets/images/large-waves.svg" alt="waves" decoding="async" aria-hidden="true" width="625" height="952">
+        <img src="{waves.icon}" alt="{waves.altText}" decoding="async" aria-hidden="true" width="625" height="952">
     </picture>
 </section>
