deb05a8f64e67c2f2ff0a132aeb333ff8e97f71b
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-01-22 00:36:59 +1100

    update with content replacement pattern

--- layouts/components/contact_486.svelte
+++ layouts/components/contact_486.svelte
@@ -1,48 +1,51 @@
+<script>
+    export let title1, title2, description, question, cta, phone, email, form;
+</script>
 <!-- ============================================ -->
-<!--                    Form                      -->
+<!--                    Form  486                 -->
 <!-- ============================================ -->
 
 <section id="cs-contact-486">
     <div class="cs-container">
         <div class="cs-content">
-            <h2 class="cs-title">Talk to us about your <span class="cs-color">fencing plan!</span></h2>
+            <h2 class="cs-title">{title1} <span class="cs-color">{title2}</span></h2>
             <p class="cs-text">
-                We would love to hear about your project and our team is ready to discuss your needs, provide a free consultation, and offer expert guidance to bring your vision to life. Contact us today and let us transform your home with our professional service. Please fill the form on the right side to schedule a call or discussion and meeting on the your convenient time.
+                {description}
             </p>
-            <span class="cs-heading">Any problem about your booking?</span>
-            <span class="cs-heading">You can contact us on :</span>
-            <a href="tel:219-555-4545" class="cs-link">0416 186 911</a>
-            <a href="mailto:inspiredfencing@outlook.com" class="cs-link">inspiredfencing@outlook.com</a>
+            <span class="cs-heading">{question}</span>
+            <span class="cs-heading">{cta}</span>
+            <a href="tel:{phone.number}" class="cs-link">{phone.number}</a>
+            <a href="mailto:{email.address}" class="cs-link">{email.address}</a>
         </div>
         <!--Form-->
         <form id="cs-form-486" name="Contact Form" method="post">
-            <h3 class="cs-h3">Get In Touch</h3>
+            <h3 class="cs-h3">{form.title}</h3>
             <label class="cs-label">
-                Name
-                <input class="cs-input" required type="text" id="name-486" name="name" placeholder="Name">
+                {form.name}
+                <input class="cs-input" required type="text" id="name-486" name="name" placeholder="{form.name}">
             </label>
             <label class="cs-label">
-                Phone
-                <input class="cs-input" required type="text" id="phone-486" name="phone" placeholder="Phone">
+                {form.phone}
+                <input class="cs-input" required type="text" id="phone-486" name="phone" placeholder="{form.phone}">
             </label>
             <label class="cs-label">
-                Email
-                <input class="cs-input" required type="text" id="email-486" name="email" placeholder="Email">
+                {form.email}
+                <input class="cs-input" required type="text" id="email-486" name="email" placeholder="{form.email}">
             </label>
             <label class="cs-label">
-                How Did You Find Us
-                <input class="cs-input" type="text" id="find-486" name="find-us" placeholder="How did you find us?">
+               {form.label1}
+                <input class="cs-input" type="text" id="find-486" name="find-us" placeholder="{form.label1}">
             </label>
             <label class="cs-label cs-label-message">
-                What do you need help with?
+                {form.label2}
                 <select name="cars" id="cs-select-486">
-                    <option value="full renovation">New fence / gate</option>
-                    <option value="sod install">Fence / gate repair or replacement</option>
-                    <option value="yard cleanup">Fence and Waste Removal</option>
-                    <option value="landscape design">Handyman and Garden Maintenance</option>
+                    <option value="full renovation">{form.option1}</option>
+                    <option value="sod install">{form.option2}</option>
+                    <option value="yard cleanup">{form.option3}</option>
+                    <option value="landscape design">{form.option4}</option>
                 </select>
             </label>
-            <button class="cs-button-solid cs-submit" type="submit">Book a Consult</button>
+            <button class="cs-button-solid cs-submit" type="submit">{form.button}</button>
         </form>
     </div>
 </section>
