848e0693faef8ba03bbf7ad45eaa6f2645c70f83
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-01-22 13:04:58 +1100

    add replacement pattern

--- layouts/components/cta_697.svelte
+++ layouts/components/cta_697.svelte
@@ -1,3 +1,6 @@
+<script>
+    export let title, button, waves;
+</script>
 <!-- ============================================ -->
 <!--                     CTA                      -->
 <!-- ============================================ -->
@@ -5,11 +8,11 @@
 <section id="cta-697">
     <div class="cs-container">
         <div class="cs-content">
-            <h2 class="cs-title">Have Doubts? Get Your Queries Solved by Our Experts</h2>
-            <a href="." class="cs-button-solid">Get In Touch</a>
+            <h2 class="cs-title">{title}</h2>
+            <a href="{button.url}" class="cs-button-solid">{button.title}</a>
         </div>
     </div>
     <!--SVG Waves Background-->
-    <img class="cs-background" src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images%2FGraphics%2Fwaves-3.svg" alt="wave graphic" width="1920" height="318" loading="lazy" decoding="async" aria-hidden="true">
+    <img class="cs-background" src="{waves.url}" alt="{waves.alt}" width="1920" height="318" loading="lazy" decoding="async" aria-hidden="true">
 </section>
                                 
