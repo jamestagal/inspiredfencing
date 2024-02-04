055489bc7bd47b6b137aa1ed1383c6bb5d907b89
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-02-05 08:53:22 +1100

    update reviews with imported content from testimonials

--- layouts/components/reviews_607.svelte
+++ layouts/components/reviews_607.svelte
@@ -1,43 +1,50 @@
+<script>
+  export let subtitle, title, description, button, allContent;
+  let testimonials = allContent.find(
+    (content) => content.type === "testimonials"
+  );
+  // import Banner from "../components/banner_559.svelte";
+</script>
+
 <!-- ============================================ -->
 <!--                   Reviews  607               -->
 <!-- ============================================ -->
-
+<!-- <Banner /> -->
 <section id="reviews-607">
-    <div class="cs-container">
-        <div class="cs-content">
-            <span class="cs-topper">Testimonials</span>
-            <h2 class="cs-title">Trusted by Thousand of home oweners & Companies</h2>
-            <p class="cs-text">
-                We don't just boast about our work; our clients place their trust in us. See for yourself what a few of our customers have shared about their interactions with Inspired Fencing.
-            </p>
-        </div>
-        <ul class="cs-card-group">
-            <li class="cs-item">
-                <img class="cs-icon" src="media/svg/green-quote-marks.svg" alt="quote icon" aria-hidden="true" width="40" height="38" loading="lazy" decoding="async">
-                <h3 class="cs-h3">Loving the color!</h3>
-                <p class="cs-item-text">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores iste vitae, aliquam temporibus modi libero dolorem rerum? Dolore soluta eligendi mollitia. Doloribus et exercitationem cupiditate?
-                </p>
-                <span class="cs-name">Daniel</span>
-            </li>
-            <li class="cs-item">
-                <img class="cs-icon" src="media/svg/green-quote-marks.svg" alt="quote icon" aria-hidden="true" width="40" height="38" loading="lazy" decoding="async">
-                <h3 class="cs-h3">Affordable</h3>
-                <p class="cs-item-text">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores iste vitae, aliquam temporibus modi libero dolorem rerum? Dolore soluta eligendi mollitia. Doloribus et exercitationem cupiditate?
-                </p>
-                <span class="cs-name">Daniel</span>
-            </li>
-            <li class="cs-item">
-                <img class="cs-icon" src="media/svg/green-quote-marks.svg" alt="quote icon" aria-hidden="true" width="40" height="38" loading="lazy" decoding="async">
-                <h3 class="cs-h3">Professional and the best!</h3>
-                <p class="cs-item-text">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores iste vitae, aliquam temporibus modi libero dolorem rerum? Dolore soluta eligendi mollitia. Doloribus et exercitationem cupiditate?
-                </p>
-                <span class="cs-name">Daniel</span>
-            </li>
-        </ul>
-        <a href="." class="cs-button-solid">Read More Reviews</a>
+  <div class="cs-container">
+    <div class="cs-content">
+      <span class="cs-topper">{subtitle}</span>
+      <h2 class="cs-title">{title}</h2>
+      <p class="cs-text">
+        {description}
+      </p>
     </div>
+    <ul class="cs-card-group">
+      {#each testimonials.fields.quote as testimonial}
+        {#if testimonial.id === "1" || testimonial.id === "2" || testimonial.id === "3"}
+          <li class="cs-item">
+            <img
+              class="cs-icon"
+              src="media/svg/green-quote-marks.svg"
+              alt="quote icon"
+              aria-hidden="true"
+              width="40"
+              height="38"
+              loading="lazy"
+              decoding="async"
+            />
+            <h3 class="cs-h3">{testimonial.title}</h3>
+            <p class="cs-item-text">
+              {@html testimonial.description
+                ?.substring(0, 136)
+                .replace(/(<([^>]+)>)/gi, "") + " ..."}
+            </p>
+            <span class="cs-name">{testimonial.name}</span>
+            <span class="cs-job">{testimonial.location}</span>
+          </li>
+        {/if}
+      {/each}
+    </ul>
+    <a href={button.url} class="cs-button-solid">{button.title}</a>
+  </div>
 </section>
-                                
