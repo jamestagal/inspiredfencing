db917f59e04d4f6eac33d1da16d7cb0f1b5e0af4
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-02-23 22:41:43 +1100

    add baseurl forward slash

--- layouts/global/head.svelte
+++ layouts/global/head.svelte
@@ -5,24 +5,42 @@
 <head>
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0" />
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1, maximum-scale=1.0"
+  />
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <!--Swap our for actual link-->
-  <link rel="canonical" href="https://inspiredfencing.com.au/">
-  <meta name="keywords" content="Fencing Expert, Fencing, Gates, Colorbond Fencing, Fence Repair, Fence Removal, Fence Contractors, Fence Replacement, Mornington Peninsula, Somerville" />
+  <link rel="canonical" href="https://inspiredfencing.com.au/" />
+  <meta
+    name="keywords"
+    content="Fencing Expert, Fencing, Gates, Colorbond Fencing, Fence Repair, Fence Removal, Fence Contractors, Fence Replacement, Mornington Peninsula, Somerville"
+  />
   <meta
     name="description"
     content="Inspired Fencing is a family owned and operated fencing contractor builting bespoke fence installations and repair services at affordable prices based in Somerville on Mornington Peninsula."
   />
-  <title>Fencing Expert in Mornington Peninsula & South East Melbourne | Inspired Fencing</title>
-
-    <!--Social Media Display-->
-    <meta property="og:title" content="Inspired Fencing | Fencing Expert in Mornington Peninsula & South East Melbourne"/>
-    <meta property="og:description" content="Inspired Fencing is a family owned and operated fencing contractor builting bespoke fence installations and repair services at affordable prices based in Somerville on Mornington Peninsula."/>
-    <meta property="og:type" content="website" />
-    <meta property="og:url" content="https://inspiredfencing.com.au/"/>
-    <meta property="og:image" content="media/images/InspiredFencing-logo.jpeg"/>
-    <meta property="og:image:secure_url" content="media/images/InspiredFencing-logo.jpeg"/>
+  <title
+    >Fencing Expert in Mornington Peninsula & South East Melbourne | Inspired
+    Fencing</title
+  >
+  <base href={env.baseurl} />
+  <!--Social Media Display-->
+  <meta
+    property="og:title"
+    content="Inspired Fencing | Fencing Expert in Mornington Peninsula & South East Melbourne"
+  />
+  <meta
+    property="og:description"
+    content="Inspired Fencing is a family owned and operated fencing contractor builting bespoke fence installations and repair services at affordable prices based in Somerville on Mornington Peninsula."
+  />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://inspiredfencing.com.au/" />
+  <meta property="og:image" content="media/images/InspiredFencing-logo.jpeg" />
+  <meta
+    property="og:image:secure_url"
+    content="media/images/InspiredFencing-logo.jpeg"
+  />
 
   <script type="module" src={`${env.entrypointJS}/core/main.js`}></script>
   <script type="module" src="js/nav.js"></script>
