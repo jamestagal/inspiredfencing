c8c5cfc076a1a22a5a006b9fdc6e2206d819fa68
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-02-29 22:30:04 +1100

    update formatting

--- layouts/global/head.svelte
+++ layouts/global/head.svelte
@@ -3,12 +3,10 @@
 </script>
 
 <head>
+  <!--Standard meta tags-->
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta
-    name="viewport"
-    content="width=device-width, initial-scale=1"
-  />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <!--Swap our for actual link-->
   <link rel="canonical" href="https://inspiredfencing.com.au" />
@@ -41,6 +39,32 @@
     property="og:image:secure_url"
     content="media/images/InspiredFencing-logo.jpeg"
   />
+  <!--Favicons-->
+  <link
+    rel="apple-touch-icon"
+    sizes="144x144"
+    href="media/favicon/apple-touch-icon.png"
+  />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="32x32"
+    href="media/favicon//favicon-32x32.png"
+  />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="16x16"
+    href="media/favicon//favicon-16x16.png"
+  />
+  <link rel="manifest" href="media/favicon//site.webmanifest" />
+  <link
+    rel="mask-icon"
+    href="media/favicon//safari-pinned-tab.svg"
+    color="#5bbad5"
+  />
+  <meta name="msapplication-TileColor" content="#00a300" />
+  <meta name="theme-color" content="#ffffff" />
 
   <script type="module" src={`${env.entrypointJS}/core/main.js`}></script>
   <script type="module" src="js/nav.js"></script>
